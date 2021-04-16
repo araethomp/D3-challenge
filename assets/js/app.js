785e44f27b00fe58e01967cb485ebe4c31e1e77b
araethomp <angela.r.thomp@gmail.com>
2021-04-16 11:31:21 -0700

    set dimensions and margins

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
-// @TODO: YOUR CODE HERE!
+// Set dimensions and margins
+const margin = { top: 10, right: 30, bottom: 30, left: 60 };
+const innerWidth = 460 - margin.left - margin.right;
+const innerHeight = 400 - margin.top - margin.bottom;
+
+
+
+// Select tag from index
+let selectTag = d3.select("#scatter");
+
+// Pull in data.csv
+d3.csv("assets/data/data.csv", data => {
+  console.log(data);
+})
