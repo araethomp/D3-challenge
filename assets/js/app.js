575857125ed5e58fed040c7f687824d68e8b629a
araethomp <angela.r.thomp@gmail.com>
2021-04-16 12:07:51 -0700

    adding x axis and y axis

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,30 @@ const margin = { top: 10, right: 30, bottom: 30, left: 60 };
 const innerWidth = 460 - margin.left - margin.right;
 const innerHeight = 400 - margin.top - margin.bottom;
 
-
-
-// Select tag from index
-let selectTag = d3.select("#scatter");
+// Select tag from index, append svg object
+let svgObject = d3.select("#scatter")
+  .append("svgObject")
+  .attr("width", innerWidth + margin.left + margin.right)
+  .attr("height", innerHeight + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // Pull in data.csv
 d3.csv("assets/data/data.csv", data => {
-  console.log(data);
+  //console.log(data);
 })
+
+// X axis
+let x = d3.scaleLinear()
+  .domain([0, 4000])
+  .range([0, innerWidth]);
+svgObject.append("g")
+  .attr("transform", "translate(0," + innerHeight + ")")
+  .call(d3.axisBottom(x));
+
+// Y axis
+let y = d3.scaleLinear()
+  .domain([0, 500000])
+  .range([innerHeight, 0]);
+svgObject.append("g")
+  .call(d3.axisLeft(y));
