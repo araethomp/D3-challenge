c3e6276c57162b553b3a769f329e3c2dba0c2123
araethomp <angela.r.thomp@gmail.com>
2021-04-20 11:04:55 -0700

    Changing approaches

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,65 @@
-// Set dimensions and margins
-const margin = { top: 10, right: 30, bottom: 30, left: 60 };
-const innerWidth = 460 - margin.left - margin.right;
-const innerHeight = 400 - margin.top - margin.bottom;
+// Set up margins
+const svgWidth = 960;
+const svgHeight = 500;
+
+const margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+const width = svgWidth - margin.left - margin.right;
+const height = svgHeight - margin.top - margin.bottom;
 
 // Select tag from index, append svg object
-let svgObject = d3.select("#scatter")
-  .append("svgObject")
-  .attr("width", innerWidth + margin.left + margin.right)
-  .attr("height", innerHeight + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+let svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
 
-// Pull in data.csv
-d3.csv("assets/data/data.csv", data => {
-  //console.log(data);
-})
+// Append SVG group
+const chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// X axis
-let x = d3.scaleLinear()
-  .domain([0, 4000])
+  // // Initialize Params
+  let chosenXAxis = "poverty";
+
+// Function that updates xScale upon click
+function xScale(povertyData, chosenXAxis) {
+  const xLinearScale = d3.scaleLinear()
+  .domain([d3.min(povertyData, d => d[chosenXAxis]) * 0.8,
+    d3.max(povertyData, d => d[chosenXAxis]) * 1.2
+  ])
   .range([0, innerWidth]);
-svgObject.append("g")
-  .attr("transform", "translate(0," + innerHeight + ")")
-  .call(d3.axisBottom(x));
+  return xLinearScale;
+}
 
-// Y axis
-let y = d3.scaleLinear()
-  .domain([0, 500000])
-  .range([innerHeight, 0]);
-svgObject.append("g")
-  .call(d3.axisLeft(y));
+
+//
+// // Pull in data.csv
+// d3.csv("assets/data/data.csv", data => {
+//   //console.log(data);
+// })
+//
+// // X axis
+// let x = d3.scaleLinear()
+//   .domain([0, 4000])
+//   .range([0, innerWidth]);
+// svg.append("g")
+//   .attr("transform", "translate(0," + innerHeight + ")")
+//   .call(d3.axisBottom(x));
+//
+// // Y axis
+// let y = d3.scaleLinear()
+//   .domain([0, 500000])
+//   .range([innerHeight, 0]);
+// svg.append("g")
+//   .call(d3.axisLeft(y));
+//
+// // Dots
+// svg.append("g")
+//   .selectAll("dot")
+//   .enter()
+//   .append("circle")
+//   .attr("cx", function (d) { return x(d.)})
