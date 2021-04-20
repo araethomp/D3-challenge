9ed44221c1c35108490788291c0481496726387f
araethomp <angela.r.thomp@gmail.com>
2021-04-20 11:24:57 -0700

    Added mouseover and mouseout

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,7 +25,7 @@ const chartGroup = svg.append("g")
   // // Initialize Params
   let chosenXAxis = "poverty";
 
-// Function that updates xScale upon click
+// updates xScale upon click on axis label
 function xScale(povertyData, chosenXAxis) {
   const xLinearScale = d3.scaleLinear()
   .domain([d3.min(povertyData, d => d[chosenXAxis]) * 0.8,
@@ -35,11 +35,54 @@ function xScale(povertyData, chosenXAxis) {
   return xLinearScale;
 }
 
+// updates xAxis upon click on axis label
+function renderAxes(newXScale, xAxis) {
+  const bottomAxis = d3.axisBottom(newXScale);
+  xAxis.transition()
+    .duration(1000)
+    .call(bottomAxis);
+  return xAxis;
+}
 
-//
-// // Pull in data.csv
-// d3.csv("assets/data/data.csv", data => {
-//   //console.log(data);
+// updates circles group with transition to new circles
+function renderCircles(circlesGroup, newXScale, chosenXAxis) {
+  circlesGroup.transition()
+    .duration(1000)
+    .attr("cx", d => newXScale(d[chosenXAxis]));
+  return circlesGroup;
+}
+
+// updates circles group with new tooltip
+function updateToolTip(chosenXAxis, circlesGroup) {
+  let label;
+  if (chosenXAxis === "poverty") {
+    label = "Poverty:";
+  }
+  else if (chosenXAxis === "age") {
+    label = "Age:";
+  }
+  else { label = "Household Income:";
+  }
+
+  let toolTip = d3.tip()
+    .attr("class", "tooltip")
+    .offset([80, -60])
+    .html(d => `${d.state}<br>${label} ${d[chosenXAxis]}`);
+
+  circlesGroup.call(tooltip);
+
+  circlesGroup.on("mouseover", function(data) {
+    toolTip.show(data);
+  })
+    .on("mouseout", function(data) {
+      toolTip.hide(data);
+  })
+  return circlesGroup;
+}
+
+// Pull in data.csv
+d3.csv("assets/data/data.csv", data => {
+  console.log(data);
 // })
 //
 // // X axis
